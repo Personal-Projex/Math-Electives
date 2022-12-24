70b1448b0cb4e38a59bd88a2e83bcbd8171c13c7
Shzmj <109339363+Shzmj@users.noreply.github.com>
2022-12-24 13:21:03 +1100

    Implemented helper function to use the JSON file created by webscraper to populate courses on homepage

--- react/src/components/courses.js
+++ react/src/components/courses.js
@@ -1,79 +1,28 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import data from '../courses.json';
 
 export default function Courses() {
-    return (
-        <div className="course-box-container">
-            <div className="course-box">
-                <div className="box-top">
-                    <div className="profile">
-                        <div className="profile-img">
-                            <img alt="" src="images/sigma_logo_1.png" />
-                        </div>
-                        <div className="course">
-                            <strong>MATH1131</strong>
-                            <span>Mathematics 1A</span>
-                        </div>
-                    </div>
-                    <div className="reviews">
-                        <i className="fas fa-star fa-lg"></i>
-                        <i className="fas fa-star fa-lg"></i>
-                        <i className="fas fa-star fa-lg"></i>
-                        <i className="fas fa-star fa-lg"></i>
-                        <i className="far fa-star fa-lg"></i>
-                    </div>
-                </div>
-                <div className="terms-run">
-                    <span className="term">
-                        Term 1
-                    </span>
-                    <span className="term">
-                        Term 2
-                    </span>
-                    <span className="term">
-                        Term 3
-                    </span>
-                </div>
-            </div>
+    const handleTerms = (arr) => {
+        return (
+            arr.map((term) => {
+                return (term !== "" ? <span className="term">{term}</span> : null)
+            })
+        )
+    }
 
-            <div className="course-box">
-                <div className="box-top">
-                    <div className="profile">
-                        <div className="profile-img">
-                            <img alt="" src="images/sigma_logo_1.png" />
-                        </div>
-                        <div className="course">
-                            <strong>MATH1141</strong>
-                            <span>Higher Mathematics 1A</span>
-                        </div>
-                    </div>
-                    <div className="reviews">
-                        <i className="fas fa-star fa-lg"></i>
-                        <i className="fas fa-star fa-lg"></i>
-                        <i className="fas fa-star fa-lg"></i>
-                        <i className="fas fa-star fa-lg"></i>
-                        <i className="far fa-star fa-lg"></i>
-                    </div>
-                </div>
-                <div className="terms-run">
-                    <span className="term">
-                        Term 1
-                    </span>
-                    <span className="term">
-                        Term 3
-                    </span>
-                </div>
-            </div>
-            <div className="course-box">
-                <Link to="/MATH2901" className='link'>
+    const courses = data.map((course, pos) => {
+        return (
+            <div className="course-box" key={pos}>
+                <Link to={"/" + course.code} className='link'>
                     <div className="box-top">
                         <div className="profile">
                             <div className="profile-img">
                                 <img alt="" src="images/sigma_logo_1.png" />
                             </div>
                             <div className="course">
-                                <strong>MATH2901</strong>
-                                <span>Higher Theory of Statistics</span>
+                                <strong>{course.code}</strong>
+                                <span>{course.name}</span>
                             </div>
                         </div>
                         <div className="reviews">
@@ -85,12 +34,14 @@ export default function Courses() {
                         </div>
                     </div>
                     <div className="terms-run">
-                        <span className="term">
-                            Term 2
-                        </span>
+                        {handleTerms([course.term1, course.term2, course.term3])}
                     </div>
-                </Link>
-            </div>
+                </Link >
+            </div >)
+    })
+    return (
+        <div className="course-box-container">
+            {courses}
         </div>
     )
 }
--- react/src/components/review-page.js
+++ react/src/components/review-page.js
@@ -5,7 +5,14 @@ import SearchReviews from './search-review';
 import ReviewBox from './review-box';
 import './review.css';
 
+function componentDidMount() {
+    window.scrollTo(0, 0);
+}
+
 export default function ReviewPage() {
+    // make sure that when the react component is loaded it will reset to the top 
+    // (some issue with router-dom)
+    componentDidMount();
     return (
         <div className='body-reviews'>
             <NavBar />
