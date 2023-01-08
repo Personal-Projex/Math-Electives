fb8ed87ca4421e8af16d618263a399405b9e044c
Shubh <Shu8h1234@gmail.com>
2023-01-08 20:17:40 +1100

    Set up basic back-end endpoints and started adding functionality to register and login buttons

--- react/src/components/navbar.js
+++ react/src/components/navbar.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Register from './Register';
 
 export default function nav() {
     return (
         <div className="header">
             <a href="/" className="link">Math-Electives</a>
+            <Register/>
             <button className="link">Login</button>
         </div>
     )
