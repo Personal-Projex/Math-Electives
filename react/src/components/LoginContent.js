5d52309ee1323561b5c17c3dbd973a85ce78d2ea
Shubh <Shu8h1234@gmail.com>
2023-01-10 18:13:26 +1100

    Set up backend for login button

--- react/src/components/LoginContent.js
+++ react/src/components/LoginContent.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const LoginContent = props => {
 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function submitHandler(e) {
+        e.preventDefault();
+
+        const user = {
+            username: username,
+            password: password,
+        }
+
+        const response = await fetch('http://127.0.0.1:8000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+
+        const returnData = await response.json();
+        console.log(returnData);
+
+    } 
+
     return (
         <div className="register-form">
-            <form action="">
+            <form action="" onSubmit={submitHandler}>
                 <label>Username</label>
                 <div>
-                    <input type="text" placeholder='Username' className="form-text"/>
+                    <input type="text" placeholder='Username' className="form-text"  onChange={e=>setUsername(e.target.value)}/>
                 </div>
                             
                 <label>Password</label>
                 <div>
-                    <input type="password" placeholder='Password' className="form-text"/>
+                    <input type="password" placeholder='Password' className="form-text"  onChange={e=>setPassword(e.target.value)}/>
                 </div>
                 <button type='submit'>Submit</button>
             </form>
