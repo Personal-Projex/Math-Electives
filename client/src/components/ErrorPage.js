da0687e46fcaf68d0400868aa83d3d93fe635fb9
Shubh <Shu8h1234@gmail.com>
2023-01-31 18:52:13 +1100

    Forced the cogwheel to render with useEffect, in order for it to be seen spinning as soon as the page loads

--- client/src/components/ErrorPage.js
+++ client/src/components/ErrorPage.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from 'react'
 import './ErrorPage.css'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
 
 const ErrorPage = props => {
-      
-    return (
-        <>
-        <div className="container">
-            <h2 className="notFoundTxt">Oops! Page not found</h2>
+  const [isLoading, setLoading] = useState(false);
 
-            <h1>
-                <span className="num">4 </span>
-                <span className="fas fa-cog"></span>
-                <span className="num"> 4</span>
-            </h1>
+  // useEffect triggers a re-render as soon as the page loads
+  // by setting the isLoading state to true using setLoading(true).
 
-            <p className="pageTxt">We can't find the page you're looking for.</p>
-            <a href="/"> Go back home</a>
-        </div>
-        </>
-    )
+  // The useEffect hook is passed an empty dependency array [] so
+  // that it only runs once on the initial render
+  
+  useEffect(() => {
+    setLoading(true);
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <h2 className="notFoundTxt">Oops! Page not found</h2>
+        <h1>
+          <span className="num">4 </span>
+          <FontAwesomeIcon icon={faCog} spin={isLoading} className={isLoading ? 'fa-spin' : ''}/>
+          <span className="num"> 4</span>
+        </h1>
+        <p className="pageTxt">We can't find the page you're looking for.</p>
+        <a href="/"> Go back home</a>
+      </div>
+    </>
+  )
 }
 export default ErrorPage;
