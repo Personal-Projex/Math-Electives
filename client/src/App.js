d74942740cdb476861e73de8e19fe28ff0a879d5
Shzmj <109339363+Shzmj@users.noreply.github.com>
2023-02-08 09:23:17 +1100

    Trying to fix review text

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 import CoursePage from './components/review-page';
 import Home from './components/Home';
@@ -12,7 +12,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/review/:id" element={<CoursePage />} />
         <Route exact path="/404-page" element={<ErrorPage />} />
-        <Route path="/*" element={<ErrorPage />} />
+        <Route path="/*" element={<Navigate to="/404-page" replace />} />
       </Routes>
     </div>
   );
