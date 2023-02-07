1e8168a8adf7a51d96495dc90b78315bbc49b8f9
Shzmj <109339363+Shzmj@users.noreply.github.com>
2023-02-08 01:11:33 +1100

    Reverted back changes to 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,11 @@ import ErrorPage from "./components/ErrorPage";
 function App() {
   return (
     <div className="App">
-      {/* <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/review/:id" element={<CoursePage />} />
-        <Route exact path="/404-page" element={<ErrorPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes> */}
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/review/:id" element={<CoursePage />} />
         <Route exact path="/404-page" element={<ErrorPage />} />
-        <Route element={<ErrorPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </div>
   );
