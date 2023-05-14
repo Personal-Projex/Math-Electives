e11bb32967c28302bb9c67429a865669772c5953
Shaam <shaamjevan.sj@gmail.com>
2023-05-14 11:35:56 +1000

    Added login error message before user writes out the whole review

--- client/src/components/add-review.js
+++ client/src/components/add-review.js
@@ -5,13 +5,33 @@ import AddReviewBox from './add-review-box';
 
 export default function AddReview(props) {
     const [isOpen, setIsOpen] = useState(false);
+    const [alert, setAlert] = useState(false);
 
     const togglePopup = () => {
-        setIsOpen(!isOpen);
+        const token = sessionStorage.getItem('token');
+        // checking if user not logged in before reviewing
+        if (token === null || token === '') {
+            document.documentElement.style.setProperty('--timerBarColour', 'red');
+            setAlert(!alert);
+            // Make the alert disappear after 2 seconds
+            setTimeout(() => {
+                setAlert(alert => !alert);
+            }, 2000);
+        } else {
+            setIsOpen(!isOpen);
+        }
     }
 
     return (
         <>
+            {alert &&
+                <div>
+                    <div className="timer-bar"></div>
+                    <div className="alert-box">
+                        <p className="alert">Please login before reviewing</p>
+                    </div>
+                </div>
+            }
             <div className='add-review-box'>
                 <button className='add-review-button' onClick={togglePopup}>
                     <FontAwesomeIcon icon={faPen}></FontAwesomeIcon>
