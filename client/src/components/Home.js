7b9ba3fd367384602bbd7aeb39793d0184a9369a
Shaam <shaamjevan.sj@gmail.com>
2023-07-16 14:30:03 +1000

    Changed loader text between first render and normal reloads

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ScaleLoader from "react-spinners/ScaleLoader";
 import sessionStorage from 'sessionstorage';
 
@@ -7,7 +7,7 @@ import NavBar from './navbar';
 import './home.css';
 
 export default function Home() {
-    const [loading, setLoading] = useState(sessionStorage.getItem('loadingCourses') !== 'false');
+    const [loading, setLoading] = useState(true);
 
     const coursesReceived = () => {
         setLoading(false);
@@ -19,7 +19,16 @@ export default function Home() {
             {loading && (
                 <div className="loader-container">
                     <ScaleLoader color={"orange"} loading={loading} size={80} />
-                    <h1 className='loader-text'>Please wait while we compute 100 digits of Pi...</h1>
+                    {sessionStorage.getItem('loadingCourses') !== 'false' &&
+                        (
+                            < h1 className='loader-text'>Please wait while we compute 100 digits of Pi...</h1>
+                        )
+                    }
+                    {sessionStorage.getItem('loadingCourses') === 'false' &&
+                        (
+                            < h1 className='loader-text'>Loading courses...</h1>
+                        )
+                    }
                 </div>
             )}
 
