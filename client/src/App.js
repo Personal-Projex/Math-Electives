9237f58fedd90b4e375b34882c30ed54dd18a9d5
Shaam <shaamjevan.sj@gmail.com>
2023-07-29 00:48:23 +1000

    Changed Route path

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/404-page" element={<ErrorPage />} />
-        <Route exact path="/review/:id" element={<ErrorPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/review/:id" element={<CoursePage />} />
+        <Route path="/404-page" element={<ErrorPage />} />
         <Route path="*" element={<Navigate to="/404-page" replace />} />
       </Routes>
     </div>
