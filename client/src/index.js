28cbcb03ad8ee07919157fe9c820308301ec36cd
Shaam <shaamjevan.sj@gmail.com>
2023-07-29 00:04:58 +1000

    Fixed routing issues - replaced hashRouter with browserRouter

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { BrowserRouter } from 'react-router-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 if (process.env.NODE_ENV === 'production') disableReactDevTools();
